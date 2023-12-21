f2dcd206f29dff672e58fc0dd0a3223ad2edc54a
X L <xl544@cornell.edu>
2023-12-20 22:59:12 -0800

    Add consolidation

--- trading/binance_tracker.py
+++ trading/binance_tracker.py
@@ -9,3 +9,37 @@ OR just use 1X so can hold if it doesn't play out.
 every 12 - 24h using the 30m chart.
 
 """
+
+from binance.spot import Spot
+
+client = Spot()
+
+#
+def track_top_breakthrough():
+    # for each candidate, do:
+    pass
+
+def is_consolidating(prices, percentage, days=0):
+    # scan for the past d days for daily high/low
+    resistance = 0 # also record the highest date(s)
+    support = 0  # also record the lowest date(s)
+    for price in prices:
+        resistance = max(resistance, price)
+        support = min(resistance, price)
+    bandwidth = resistance - support
+    highest_days = []
+    lowest_days = []
+    # consolidation should satisfy the following:
+    # 1. Today's highest <= resistance && lowest >= support
+    # 2. at least two dates close to resistance or support,
+    # i.e. |price - resistance| / resistance <= X% (or support)
+    # We may need to tighten this rule to filter out fake ones.
+    if prices[-1] > resistance or prices[-1] < support:
+        return False
+    highest_count, lowest_count = 0, 0
+    for price in prices:
+        if (resistance - price) / resistance <= percentage:
+            highest_count += 1
+        elif (price - support) / support <= percentage:
+            lowest_count += 1
+    return highest_count >= 2 or lowest_count >= 2
