85803fbc5e6e9c4efbf6247ec0445c669ea1994b
X L <xl544@cornell.edu>
2024-01-04 19:54:24 -0800

    Add account info

---
+++ trading/binance_test.py
@@ -0,0 +1,31 @@
+"""
+Test functions in binance_tracker.py
+"""
+
+import unittest
+
+from binance_tracker import purchase_coin
+
+
+
+class IntegerArithmeticTestCase(unittest.TestCase):
+    def testAdd(self):  # test method names begin with 'test'
+        self.assertEqual((1 + 2), 3)
+        self.assertEqual(0 + 1, 1)
+    def testMultiply(self):
+        self.assertEqual((0 * 10), 0)
+        self.assertEqual((5 * 8), 40)
+
+# class TestBinanceTracker(unittest.TestCase):
+#     def test_purchase_coin(self):
+#         self.assertEqual(purchase_coin('BTCUSDT', 0.1), 0.1)
+
+# Test the function purchase_coin in binance_tracker.py
+def test_purchase_coin():
+    purchase_coin('BTCUSDT', 0.001)
+
+def main():
+    test_purchase_coin()
+
+if __name__ == "__main__":
+    main()
--- trading/binance_tracker.py
+++ trading/binance_tracker.py
@@ -1,7 +1,7 @@
 """
 1. Scan all large caps and find candidates as per
 1.1 Consolidating. Definition: In the past X days, there existed a local bottom and top.
-1.2 Add alert for the top
+1.2 Add alert for the top and for the bottom after the top is conquered.
 1.3 (important) When the top is conquered, and it lasts for ~24h, follow to join with 
 moderate leverage (e.g. 3X or use the bottom as the liquidation price)
 OR just use 1X so can hold if it doesn't play out. 
@@ -10,11 +10,46 @@ every 12 - 24h using the 30m chart.
 
 """
 
+import sys
+#sys.path.append('/Library/Frameworks/Python.framework/Versions/3.12/lib/python3.12/site-packages')
+sys.path.append('/Users/xl/Library/Python/3.9/lib/python/site-packages')
 from binance.spot import Spot
 
-client = Spot()
+api_key = 'oTcRPne8yYBkCaXrT6DItAu4dsEshAFnVjXyYyadfX6e5BFPNK4jwduu8seObvd0'
+# read from key.txt and assign the value to api_scrt
+def read_src():
+    with open('key.txt', 'r') as file:
+        return file.read().strip()
+client = Spot(api_key=api_key,api_secret=read_src())
 
 one_trillion = 1_000_000_000_000
+
+# Catch at the bottom of a bounce back, e.g. PEOPLE 1/3/2024 or UNFIUSDT 6/7/2022
+def catch_bottom():
+    pass
+
+
+# Scan all large caps and find candidates that falls back from a local top
+def find_consolidating_candidates():
+    # for each candidate, do:
+    pass
+
+# Scan all large caps and find candidates that breaks through a local top
+def find_breakthrough_candidates():
+    # for each candidate, do:
+    pass
+
+# Adjust the stop price during a blow-off top
+def adjust_stop_price():
+    pass
+
+# add alert for the top where you should enter the market
+def add_alert():
+    pass
+
+# Reorder the watchlist based on the current price
+def reorder_watchlist():
+    pass
 
 #
 def track_top_breakthrough():
@@ -68,13 +103,32 @@ def is_consolidating(prices, percentage, limit):
             lowest_count += 1
     return highest_count >= 2 or lowest_count >= 2
 
+# Given the symbol of a coin, purchase it with the given amount
+def purchase_coin(symbol, amount):
+    client.new_order(symbol=symbol, side='BUY', type='MARKET', quantity=amount)
 
-# main function
+# Given the symbol of a coin, purchase it with the given amount using limit order
+def purchase_coin_limit(symbol, amount, price):
+    client.new_order(symbol=symbol, side='BUY', type='LIMIT', quantity=amount, price=price)
+
+# Given the symbol of a coin, sell it with the given amount
+def sell_coin(symbol, amount):
+    client.new_order(symbol=symbol, side='SELL', type='MARKET', quantity=amount)
+
+def replace_order(symbol, order_id, quantity, price):
+    client.cancel_order(symbol=symbol, orderId=order_id)
+    client.new_order(symbol=symbol, side='BUY', type='LIMIT', quantity=quantity, price=price)
+
+def get_balance():
+    print(client.account_snapshot('SPOT'))
+
 def main():
     # test case for is_consolidating for BTCUSDT for the past 23 and 24 days respectively
-    print(fetch_high_low_prices('BTCUSDT', '1d', 23))
+    #print(fetch_high_low_prices('BTCUSDT', '1d', 23))
     #assert is_consolidating(fetch_high_low_prices('BTCUSDT', '1d', 23), 0.05, 23) == True
     #assert is_consolidating(fetch_high_low_prices('BTCUSDT', '1d', 24), 0.05, 24) == False
+    #get_balance()
+    get_balance()
 
 
 if __name__ == "__main__":
